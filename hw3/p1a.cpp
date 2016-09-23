627ec8693b636eb2a0991fd52929d7e28cf31502
teeseng <andydomokun@gmail.com>
2016-09-23 15:49:11 -0500

    starting hw3

---
+++ hw3/p1a.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+// Collabs: Noel, Alex, Ariya
+ 
+// ANT Lib
+long fast_power(int,int,int);
+int shanks_algo(long, long, long, long, long);
+
+// shanks_algo black box
+int shanks_algo(long g, long h, long p, long, long)
+{
+    // Find n.
+    long order;
+    for(int i = 2; i < p; i++)
+    {
+        if((p-1) % i == 0)
+        {
+            if((fast_power(g, i, p)) == 1L)
+            {
+                order = (long)i;
+                break;
+            }
+        }
+    }
+    int n = ceil(sqrt(order));
+
+    // Appending the list (baby list)
+    long baby[n];
+    for(int i = 0; i < n; i++)
+    {
+        baby[i] = fast_power(g, i, p);
+    }
+    int x = 1;
+
+    /* Calculating giant step, finding match */
+    long ginv = fast_power(g, p - 2, p); // g modulo inverse
+    for(int j = 0; j < n; j++)
+    {
+        long v1 = (h * fast_power(ginv,j * n, p)) % p;
+        int flag = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if(baby[i] == v1)
+            {
+                flag = 1;
+                x = i + (j * n);
+                break;
+            }
+        }
+        if(flag)
+            break;
+    }
+    return x;
+}
+
+// fast_power black box
+long fast_power(int g, int x, int n)
+{
+    long result = 1L;
+    g = g % n;
+    while(x > 0)
+    {
+        if(x % 2 == 1)
+        {
+            result = (result * g) % n;
+        }
+        x = x >> 1;
+        g = (g * g) % n;
+    }
+    return result;
+}
+
+
